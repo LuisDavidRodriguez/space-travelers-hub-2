c1c360d172f825fb4df9eacbc2f26eb3b91eab48
OgiogioVictor <ogiogiovictor@gmail.com>
2022-09-27 13:06:39 -0600

    add rockets

--- src/components/Rockets/Rockets.jsx
+++ src/components/Rockets/Rockets.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRockets } from '../redux/rockets/rocketSlice';
 
-const Rockets = () => (
-  <div>Rockets</div>
-);
+const Rockets = () => {
+
+  const allRockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!allRockets.length) {
+      dispatch(getRockets());
+    }
+  }, [allRockets.length]);
+  console.log(allRockets);
+
+  return (
+    <Fragment>
+    <div>Rockets</div>
+    <div>dragons display here</div>
+    </Fragment>
+  );
+}
+ 
 
 export default Rockets;
