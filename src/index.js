3bccfba728b55d9c728708e3cb2d35cb6f229885
Luis David Rodriguez Valades <luisdavidrodriguezv@gmail.com>
2022-09-27 15:13:08 -0500

    Update index.js
    Solve linters errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import './styles/_global.scss';
 import App from './components/App/App';
-import './styles/_global.scss';
 import store from './redux/configStore';
+import './styles/_global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
