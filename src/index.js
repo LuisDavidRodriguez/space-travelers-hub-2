c6925bcacb6150128b1448d9379f770c41929f6e
Luis David Rodriguez Valades <luisdavidrodriguezv@gmail.com>
2022-09-27 15:09:40 -0500

    Update index.js
    try resolve linters errors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/App/App';
 import store from './components/redux/store';
-
 import './styles/_global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,6 +13,3 @@ root.render(
     </Provider>
   </React.StrictMode>,
 );
-
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
