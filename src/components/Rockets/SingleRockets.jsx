da9b909e8c9d878ff726bf5b82acef07413eb07d
OgiogioVictor <ogiogiovictor@gmail.com>
2022-09-29 08:05:10 -0600

    fixed issues on rockets

--- src/components/Rockets/SingleRockets.jsx
+++ src/components/Rockets/SingleRockets.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import { reserveRocket, cancelRocket } from '../../redux/rockets/rocketSlice';
+import { Table, Stack } from 'react-bootstrap';
+import { Badge, Button } from 'react-bootstrap';
 
 import './Rockets.css';
 
@@ -10,20 +12,33 @@ const SingleRocket = ({ allrocekts }) => {
 
   return (
     <div>
+       <Stack className="p-3">
+          <Table responsive striped bordered hover size="sm">
+            <thead>
+              <tr>
+              <th>Image</th>
+              <th>Rockets</th>
+              <th>Description</th>
+              <th>Status</th>
+              <th> </th>
+            </tr>
+            </thead>
+
+            <tbody>
       {
         allrocekts.map((rocket) => (
-          <div key={rocket.id} className="rocket-card">
-            <div className="rocket-card-header">
-              <h3>{rocket.rocket_name}</h3>
-              <img className="rocket-image" src={rocket.flickrImages} alt={rocket.rocket_name} />
-            </div>
-            <div className="rocket-card-body">
-              {rocket.reserved ? <span className="rocket-unavailable">RESERVED</span> : ' '}
-              <p>{rocket.description}</p>
-            </div>
 
-            <div>
-              {rocket.reserved === false ? (
+            <tr key={rocket.id}>
+            <td className="fw-bold"><img className="rocket-image" src={rocket.flickrImages} alt={rocket.rocket_name} /></td>
+            <td className="fw-bold">{rocket.name}</td>
+            <td>{rocket.description}</td>
+            <td className="align-middle">
+              <Badge bg={rocket.reserved ? 'secondary' : ''} > 
+              {rocket.reserved ? <span className="rocket-unavailable">RESERVED</span> : ' ' }
+              </Badge>
+            </td>
+            <td className="align-middle">
+            {rocket.reserved === false ? (
                 <button
                   className="btn-reserve-rocket"
                   onClick={() => dispatch(reserveRocket(rocket.id))}
@@ -42,11 +57,51 @@ const SingleRocket = ({ allrocekts }) => {
                   CANCEL DRAGON
                 </button>
               )}
-            </div>
+            </td>
+            </tr>
 
-          </div>
+          
+
+          // <div key={rocket.id} className="rocket-card">
+          //   <div className="rocket-card-header">
+          //     <h3>{rocket.rocket_name}</h3>
+          //     <img className="rocket-image" src={rocket.flickrImages} alt={rocket.rocket_name} />
+          //   </div>
+          //   <div className="rocket-card-body">
+          //     {rocket.reserved ? <span className="rocket-unavailable">RESERVED</span> : ' '}
+          //     <p>{rocket.description}</p>
+          //   </div>
+
+          //   <div>
+          //     {rocket.reserved === false ? (
+          //       <button
+          //         className="btn-reserve-rocket"
+          //         onClick={() => dispatch(reserveRocket(rocket.id))}
+          //         id={rocket.id}
+          //         type="submit"
+          //       >
+          //         RESERVE DRAGON
+          //       </button>
+          //     ) : (
+          //       <button
+          //         className="btn-cancel-reservation"
+          //         onClick={() => dispatch(cancelRocket(rocket.id))}
+          //         id={rocket.id}
+          //         type="submit"
+          //       >
+          //         CANCEL DRAGON
+          //       </button>
+          //     )}
+          //   </div>
+
+          // </div>
         ))
         }
+          </tbody>
+
+</Table>
+</Stack>
+
     </div>
   );
 };
